45ff3e588184e8a69e74d555525935359afb13ac
Huden <klaud.i@mail.ru>
2023-12-15 14:29:44 +0300

    klaud 2.0

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -2,15 +2,15 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ ease: "none", duration: 2 });
 
 const tl = gsap.timeline();
-tl.from(".main__screen", {yPercent: -30})
+tl.from(".main__screen", { yPercent: -30 })
 
 
-  ScrollTrigger.create({
+ScrollTrigger.create({
     animation: tl,
     trigger: "#container",
     start: "top 20%",
     end: "120% top",
     scrub: true,
     anticipatePin: 1
-  });
+});
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-btn.onclick = function(){
+btn.onclick = function () {
     menu__open.classList.toggle('mobile-menu-side--hidden');
     header__close.classList.toggle('header--hidden');
 }
 
 const g = document.querySelectorAll('.nav__link--mobile')
 
-g.forEach(element =>{
+g.forEach(element => {
     element.addEventListener('click', () => {
         menu__open.classList.toggle('mobile-menu-side--hidden');
         header__close.classList.toggle('header--hidden');
