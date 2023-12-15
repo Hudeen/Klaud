1974259cbc3440f57ea9c1739fde5a0bcf1131a7
Huden <klaud.i@mail.ru>
2023-12-15 16:09:38 +0300

    optimized

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,10 +1,10 @@
 const isMobile = window.innerWidth <= 768; // Set isMobile to true if the window width is less than or equal to 768px
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.defaults({ ease: "none", duration: 2 });
+gsap.defaults({ ease: "none", duration: 1 });
 
 const tl = gsap.timeline();
-tl.from(".main__screen", { translateY: isMobile ? "-30%" : "-30%", duration: 1 });
+tl.from(".main__screen", { translateY: isMobile ? "-30%" : "-30%"});
 
 ScrollTrigger.create({
     animation: tl,
