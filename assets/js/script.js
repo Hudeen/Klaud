58e5584e70b932167f79a21ed585d2692d7b0a39
Huden <klaud.i@mail.ru>
2023-12-15 14:47:04 +0300

    menu fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 btn.onclick = function () {
     menu__open.classList.toggle('mobile-menu-side--hidden');
     header__close.classList.toggle('header--hidden');
+    
+    // Add or remove "overflow-hidden" class from the body element
+    document.body.classList.toggle('overflow-hidden');
 }
 
 const g = document.querySelectorAll('.nav__link--mobile')
@@ -9,5 +12,8 @@ g.forEach(element => {
     element.addEventListener('click', () => {
         menu__open.classList.toggle('mobile-menu-side--hidden');
         header__close.classList.toggle('header--hidden');
+        
+        // Add or remove "overflow-hidden" class from the body element
+        document.body.classList.toggle('overflow-hidden');
     })
 });
